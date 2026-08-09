5a62a37ec52f90a2bf8bb6dbfc57b5a345ba6950
zolotaryov27 <anton.zolotaryov27@yandex.ru>
2026-08-09 16:42:28 +0300

    add search-input

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -3,18 +3,16 @@ import { IoSearch } from "react-icons/io5";
 export default function search() {
   return (
     <form className="search-form" action="/search" method="get">
-      <div className="search_wrapper">
-        <span className="search_icon">
-          <IoSearch />
-        </span>
-        <input
-          type="search"
-          id="query"
-          name="search"
-          className="search-input"
-          placeholder="Что ищете?"
-        />
-      </div>
+      <input
+        type="search"
+        id="query"
+        name="search"
+        className="search-input"
+        placeholder="Что ищете?"
+      />
+      <span className="search_icon">
+        <IoSearch />
+      </span>
     </form>
   );
 }
